ea632832b5c0f1e74494a05b583aefc1f5f91969
zhangyu100680 <1006801972@qq.com>
2017-11-08 19:15:17 +0800

    注释语句

---
+++ src/Testif/Test01.java
@@ -0,0 +1,20 @@
+package Testif;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in=new Scanner (System.in);
+		int i=in.nextInt();
+		if(i==1){
+			System.out.println();
+		}
+		
+
+	}
+
+}
